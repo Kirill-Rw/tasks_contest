402178b36311a006ce27f0012128bb084e35ce09
Kirill <kashumilin68@gmail.com>
2025-03-29 23:17:38 +0300

    Изменил task_3.cpp 2

--- task_3.cpp
+++ task_3.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    int m;
-    cin >> m;
-    cout << m / 5;
+    int a, b;
+    cin >> a >> b;
+    int count = 0;
+    for (int i = a; i <= b; i++) {
+        if (isPrime(i) == true) {
+            count += 1;
+        }
+    }
+    cout << count;
     return 0;
 }
