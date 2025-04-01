0d1081c9b83e4d66ee2c4edf501370cc55a4f758
Kirill <kashumilin68@gmail.com>
2025-04-02 00:35:17 +0300

    задача сокращение дроби

--- task_3.cpp
+++ task_3.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
+
 using namespace std;
 
-bool isPrime(int n) {
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
+int main() {
+    int a, b, a1, b1;
+    cin >> a >> b;
+
+    bool otritsatelnoe;
+
+    if (a * b < 0) {
+        otritsatelnoe = true;
+    } else {
+        otritsatelnoe = false;
+    }
+
+    if (a < 0) {
+        a *= -1;
+    }
+    if (b < 0) {
+        b *= -1;
+    }
+
+    a1 = a;
+    b1 = b;
+
+    while (a1 != 0 and b1 != 0) {
+        if (a1 > b1) {
+            a1 %= b1;
+        } else {
+            b1 %= a1;
         }
     }
-    return true;
-}
-
-int last_num(int n) {
-    return n % 10;
-}
-
-int time(int a, int b, int c, int d) {
-    int k = a * 60 + b + c * 60 + d;
-    if (24 * 60 < k) {
-        return k - 24 * 60;
+    int c = a1 + b1;
+    a /= c;
+    b /= c;
+    if (otritsatelnoe == true) {
+        cout << "-" << a << " " << b;
+    } else {
+        cout << a << " " << b;
     }
-    return k;
-}
 
-int fib(int x) {
-    if (x == 1 || x == 2) {
-        return 1;
-    }
-    int f_x = fib(x - 1) + fib(x - 2);
-    return f_x;
-}
-
-int gcd(int a, int b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
-}
-
-int lcm(int a, int b) {
-    return (a + b) / gcd(a, b);
-}
-    return 0;
 }
